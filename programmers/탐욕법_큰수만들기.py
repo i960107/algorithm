3423c356e1bf60a79cfd70fcae78d6863adf70f0
i960107 <i960107@naver.com>
2022-02-27 23:48:56 +0900

    Add Greedy Algorithm

--- programmers/탐욕법_큰수만들기.py
+++ programmers/탐욕법_큰수만들기.py
@@ -1,24 +1,39 @@
-def solution(num: str, k: int) -> str:
+def solution(number: str, k: int) -> str:
     answer = ""
 
-    start = 0  # 지울 문자를 검색할 대상 문자의 시작 인덱스
-    end = len(num) - k  # 지울 문자를 검색할 대상 문자의 마지막 인덱스 + 1
-    cnt = len(num) - k  # 검색할 대상 문자의 수
-    while k > 0:
-        print(f'start {start}  end {end} cnt {cnt} answer {answer}')
-        max_num = num[start]
-        for x in num[start:end]:
-            if x > max_num:
-                max_num = x
-        answer += max_num
-        k -= (cnt - 1)
-        start = end
-        end = len(num) - k
-        cnt = end - start
+    # 모아진 숫자. 문자열은 immutable하기 때문에 list에 모으는게 나음.
+    collected = []
 
+    #k개 만큼 다 뺏을때 그 위치부터 마지막 글자까지 지칭하기 위해 인덱스 필요
+    for i,num in enumerate(number):
+        while
+
+
+    retrun answer
+
+    # while k > 0 and curr <= len(num) - 1:
+    #     while True:
+    #         print(f'단계{n}answer{answer} curr{num[curr]} k {k}')
+    #         n += 1
+    #         if answer and answer[-1] < num[curr]:
+    #             k -= 1
+    #             answer = answer[:-1]
+    #         elif answer and answer[-1] >= num[curr]:
+    #             answer += num[curr]
+    #             break
+    #         else:
+    #             answer += num[curr]
+    #             break
+    #     curr += 1
+    #
+    # return answer
+
+
+def solution_other(num: str, k: int) -> str:
+    answer = ""
     return answer
 
 
 print(solution("1924", 2))
 print(solution("1231234", 3))
-print(solution("775841", 4))
+print(solution("417752841", 4))
