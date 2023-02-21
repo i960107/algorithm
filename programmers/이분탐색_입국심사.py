d2d1d0ed3f57f528ba680efb9335aeb44f648fdb
i960107 <i960107@naver.com>
2023-02-22 08:39:00 +0900

    Add DP Algorithms

--- programmers/이분탐색_입국심사.py
+++ programmers/이분탐색_입국심사.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 def solution(n: int, times: list) -> int:
     sec = 0
     # 입국심사관별 작업 들어간 시간. 비어있으면 0
@@ -40,5 +43,34 @@ def solution_others(n: int, times: list) -> int:
 
 print(solution_others(6, [7, 10]))
 
+
 # set1 = set([1, 2, 2, 1]) set2 = set([2, 2])
 # print(set1.intersection(set2))
+
+def solution_practice(n: int, times: List[int]) -> int:
+    times.sort()
+    # left를 다음과 같이 지정할경우 times = [7, 14]일때 부정확한 값이 됨.
+    # 실제로 구하려면 time의 최소 공배수와 각 time의 나눗셈 결과를 해야함.
+    # left, right = times[-1] * n // len(times), times[-1] * n
+    # 조건을 만족하는 최소값
+    left, right = 0, times[-1] * n
+    answer = right
+    while left <= right:
+        mid = left + (right - left) // 2
+        # 이 시간안에 다 처리할 수 있으면:
+        # 각 심사관이 쉬지 않고 심사했을때 처리할 수 있는 사람 수, 최대로 처리 가능한 경우이기 때문에
+        # n이 이것보다 작다면, 처리할 수 있음
+        # 가장 짧게 걸리는 사람이 가장 많이 처리하는게 가장 효율적.
+        max_immigration = 0
+        for time in times:
+            max_immigration += mid // time
+        if n <= max_immigration:
+            right = mid - 1
+            answer = min(answer, mid)
+        # 이 시간안에 다 처리할 수 없으면:
+        else:
+            left = mid + 1
+    return answer
+
+
+print(solution_practice(6, [7, 10]))
--- programmers/이분탐색_징검다리.py
+++ programmers/이분탐색_징검다리.py
@@ -1,3 +1,9 @@
+from typing import List
+from itertools import combinations
+from functools import reduce
+import operator as op
+
+
 def solution(distance: int, rocks: list, n: int) -> int:
     # distance 제한 조건이 1,000,000,000으로 매우 크기때문에 이분탐색 필요하다는것 알 수 있음
     # 이분 탐색을 하려면 정렬되어있어야하고, 시작-끝 범위가 분명해야 한다.
@@ -49,4 +55,53 @@ def solution_others(distance: int, rocks: list, n: int) -> int:
     return answer
 
 
-print(solution_others(25, [2, 15, 11, 21, 17], 2))
+def solution_practice(distance: int, rocks: List[int], n: int) -> int:
+    rocks.sort()
+    left, right = 0, distance
+    rocks.append(distance)
+    answer = 0
+    while left <= right:
+        # 돌사이의 거리으 최소값
+        mid = left + (right - left) // 2
+        prev = 0
+        removed = 0
+        for rock in rocks:
+            if rock - prev < mid:
+                removed += 1
+            else:
+                prev = rock
+            if removed > n:
+                break
+        if removed <= n:
+            answer = max(answer, mid)
+            left = mid + 1
+        else:
+            right = mid - 1
+    return answer
+
+
+print(solution_practice(25, [2, 15, 11, 21, 17], 2))
+
+# print(solution_practice(25, [2, 14, 11, 21, 17, 2], 2))
+# def nCr(n: int, r: int) -> int:
+#     if n < 1 or r > n or r < 0:
+#         raise ValueError
+#     r = min(r, n - r)
+#     if dp[r] != -1:
+#         return dp[r]
+#     elif dp[r - 1] != -1:
+#         return dp[r - 1] // r * (n - r + 1)
+#
+#     numerator = reduce(op.mul, range(n, n - r, -1), 1)
+#     denominator = reduce(op.mul, range(1, r + 1), 1)
+#     return numerator // denominator
+#
+#
+# dp = [-1] * 500001
+# max_combi_len = 0
+# for r in range(1, 50001):
+#     res = nCr(50000, r)
+#     max_combi_len = max(max_combi_len, res)
+#     dp[r] = res
+#
+# print(max_combi_len)
