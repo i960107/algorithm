875688037af080afecc24613b8ecf3ef022cf1be
i960107 <i960107@naver.com>
2023-04-02 23:40:34 +0900

    Add DefenseGame Solution

---
+++ programmers/디펜스게임.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+def solution(n: int, k: int, enemy: List[int]) -> int:
+    enemies = sum(enemy)
+    if enemies > n:
+    pass
+
+
+print(solution(7, 3, [4, 2, 4, 5, 3, 3, 1]))
