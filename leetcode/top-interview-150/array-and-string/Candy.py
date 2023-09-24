84f1c3d8e340bcf15f8f79e21c995d9f513d863c
i960107 <i960107@naver.com>
2023-09-24 21:50:22 +0900

    Add Solutions

--- leetcode/top-interview-150/array-and-string/Candy.py
+++ leetcode/top-interview-150/array-and-string/Candy.py
@@ -6,7 +6,7 @@ class Solution:
     # rating이 높으면 더 많이 받아야한다.
     # 한쪽 방향으로만 신경쓰면 되나? 안됨...
     # [1,2,2]일때 두번째 2는 1보다 많이 받을 필요 없음.
-    # 같은 2여도 다른 candy받을 수 있음.
+    # O(NLogN)
     def candy(self, ratings: List[int]) -> int:
         candy = [None] * len(ratings)
 
@@ -27,23 +27,23 @@ class Solution:
 
         return sum(candy)
 
-    def candy(self, R):
-        n, ans = len(R), [1] * len(R)
-
-        for i in range(n - 1):
-            if R[i] < R[i + 1]:
-                ans[i + 1] = 1 + ans[i]
+    # O(N)
+    def candy2(self, ratings: List[int]):
+        n = len(ratings)
+        answer = [1] * n
+        for i in range(1, n):
+            if ratings[i] > ratings[i - 1]:
+                answer[i] = answer[i] + 1
 
         for i in range(n - 2, -1, -1):
-            if R[i + 1] < R[i]:
-                ans[i] = max(1 + ans[i + 1], ans[i])
-
-        return sum(ans)
+            if ratings[i] > ratings[i + 1]:
+                answer[i] = max(answer[i], answer[i + 1] + 1)
+        return sum(answer)
 
 
 s = Solution()
-print(s.candy([1, 0, 2]))
-print(s.candy([1, 2, 2]))
-print(s.candy([1, 2, 3, 4]))
-print(s.candy([4, 3, 2, 1]))
-print(s.candy([1]))
+print(s.candy2([1, 0, 2]))
+print(s.candy2([1, 2, 2]))
+print(s.candy2([1, 2, 3, 4]))
+print(s.candy2([4, 3, 2, 1]))
+print(s.candy2([1]))
--- leetcode/top-interview-150/array-and-string/TrappingRainWater.py
+++ leetcode/top-interview-150/array-and-string/TrappingRainWater.py
@@ -4,18 +4,39 @@ from typing import List
 class Solution:
     # n은 1 이상
     def trap(self, height: List[int]) -> int:
-        new_height = []
-        for h in height:
-            if new_height and new_height[-1] >= h:
-                new_height.append(new_height[-1])
+        if not height:
+            return 0
+        volume = 0
+        left, right = 0, len(height) - 1
+        left_max, right_max = height[left], height[right]
+
+        while left < right:
+            left_max = max(height[left], left_max)
+            right_max = max(height[right], right_max)
+            if left_max <= right_max:
+                volume += left_max - height[left]
+                left += 1
             else:
-                new_height.append(h)
-        print(height)
-        print(new_height)
-        result = [h1 - h2 for h1, h2 in zip(new_height, height)]
-        print(result)
-        # 언제 막아주느냔.
+                volume += right_max - height[right]
+                right -= 1
+        return volume
+
+    def trap_stack(self, height: List[int]) -> int:
+        stack = []
+        volume = 0
+        for i in range(len(height)):
+            while stack and height[i] > height[stack[-1]]:
+                top = stack.pop()
+
+                if not len(stack):
+                    break
+                distance = i - stack[-1] - 1
+                waters = min(height[i], height[stack[-1]]) - height[top]
+                volume += distance * waters
+            stack.append(i)
+        return volume
 
 
 s = Solution()
 print(s.trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]))
+print(s.trap_stack([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]))
